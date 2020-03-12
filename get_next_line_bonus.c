4239ee3afe536b0b67d4fcce2985cb743d47fe61
agourrag <gourragui.success@gmail.com>
2020-03-12 01:12:06 +0100

    working stuff

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,29 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agourrag <agourrag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 17:29:40 by agourrag          #+#    #+#             */
-/*   Updated: 2020/03/10 01:23:17 by agourrag         ###   ########.fr       */
+/*   Updated: 2020/03/12 01:09:57 by agourrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-int	free_helper(char **p1, char **p2, int res)
+int	free_helper(char **p1, int res)
 {
-	if (p1 && *p1)
+	if (*p1)
 	{
 		free(*p1);
 		*p1 = NULL;
-	}
-	if (p2 && *p2)
-	{
-		free(*p2);
-		*p2 = NULL;
 	}
 	return (res);
 }
@@ -33,24 +27,24 @@ int	copy_line(char **buffer, char *eol, char **line)
 	if (eol)
 	{
 		if (!(*line = malloc(eol - *buffer + 1)))
-			return (free_helper(buffer, NULL, -1));
+			return (free_helper(buffer, -1));
 		ft_strlcpy(*line, *buffer, eol - *buffer + 1);
 		ft_strlcpy(*buffer, eol + 1, ft_strlen(*buffer) + 1);
 		return (1);
 	}
 	*line = *buffer ? ft_strdup(*buffer) : ft_strdup("");
-	return (free_helper(buffer, NULL, 0));
+	return (free_helper(buffer, 0));
 }
 
 int	get_next_line(int fd, char **line)
 {
 	static char *buffers[10240 + 1];
 	char		*eol;
-	char		*input;
+	char		input[BUFFER_SIZE + 1];
 	char		*tmp;
 	int			size;
 
-	if (fd < 0 || fd > 10240 || !line || !(input = malloc(BUFFER_SIZE + 1)))
+	if (fd < 0 || fd > 10240 || !line)
 		return (-1);
 	if ((eol = ft_strchr(buffers[fd], '\n')))
 		return (copy_line(&buffers[fd], eol, line));
@@ -59,12 +53,12 @@ int	get_next_line(int fd, char **line)
 		input[size] = '\0';
 		tmp = buffers[fd];
 		buffers[fd] = ft_strjoin(tmp, input);
-		free_helper(&tmp, &input, 0);
+		free_helper(&tmp, 0);
 		eol = ft_strchr(buffers[fd], '\n');
 		if (eol)
 			break ;
 	}
 	if (size < 0)
-		return (free_helper(&buffers[fd], NULL, -1));
+		return (free_helper(&buffers[fd], -1));
 	return (copy_line(&buffers[fd], eol, line));
 }
